e77fa4a7c9473e5c752b5506b5a3d8fbc49e77e3
UMAIR ALI <umair.dev@hotmail.com>
2025-10-23 12:43:53 +0500

    hosted

--- app.py
+++ app.py
@@ -13,3 +13,8 @@ def health():
 @app.route('/data', methods=['POST'])
 def data():
     return jsonify({"message": "Data received"}), 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
